4775a5a450030a3dbbfecfcabc72d4ddda6c69e1
shimonzhion <brys94204@gmail.com>
2024-02-29 07:58:29 +0200

    delete inv

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
 import validation from "../../validation/validation";
